876d94a6e6989ec6b72191de5f3ae7517aae6004
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-07-16 14:48:41 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -15,4 +15,19 @@ function onSaveTime({ seconds }) {
 
 const saveCurrentTime = localStorage.getItem(playerCurrentTime);
 
-player.setCurrentTime(saveCurrentTime).then(function (playerCurrentTime) {});
+player
+  .setCurrentTime(saveCurrentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
