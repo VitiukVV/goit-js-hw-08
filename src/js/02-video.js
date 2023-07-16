8cee5ca0a5bd55bbfa88857c62f69ff97f5cee95
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-07-16 14:33:06 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const playerCurrentTime = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onSaveTime, 1000));
+
+function onSaveTime({ seconds }) {
+  const currentTime = seconds;
+  localStorage.setItem(playerCurrentTime, currentTime);
+}
+
+const saveCurrentTime = localStorage.getItem(playerCurrentTime);
+
+player.setCurrentTime(saveCurrentTime).then(function (playerCurrentTime) {});
